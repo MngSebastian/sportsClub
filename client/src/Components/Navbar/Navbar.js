634cc0041ed766c9e7b987e651d4596d126c5f0a
MngSebastian <m.sebi97@yahoo.com>
2020-02-26 16:40:43 +0100

    home page almost finished, need to fix footer, and make it 100% responsive

---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = props => {
+  return (
+    <nav className="navbar">
+      <div className="login-register">
+        <Link className="navbarItems">Home</Link>
+      </div>
+      <div className="login-register">
+        <Link className="navbarItems">Login</Link>
+        <Link className="navbarItems">Signup</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
