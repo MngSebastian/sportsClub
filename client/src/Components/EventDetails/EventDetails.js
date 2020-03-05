eae34b4b72dd9f499aca5bf1588cb9cd2f4a26d9
MngSebastian <m.sebi97@yahoo.com>
2020-03-05 11:46:54 +0100

    added scroll bar to events div

--- client/src/Components/EventDetails/EventDetails.js
+++ client/src/Components/EventDetails/EventDetails.js
@@ -4,20 +4,31 @@ import "./EventDetials.css";
 export default class EventDetails extends Component {
   state = {};
 
-  
-  
-  
-  
-  
   render() {
     return (
       <div>
         <div className="eventDetails">
           <div className="header">
-            <p>Event Name: {this.props.eventDetails.nameOfEvent}</p>
+            <p>{this.props.eventDetails.nameOfEvent}</p>
           </div>
-          <p>asd</p>
-          <button>Join</button>
+          <div>
+            <div className="usersJoining">
+              <div className="user">
+                <p>user42</p>
+              </div>
+            </div>
+            <div className="EventInfo">
+              <p>
+                {" "}
+                Description:<br></br> {this.props.eventDetails.description}
+              </p>
+              <p>
+                <br></br>
+                {this.props.eventDetails.eventTime}
+              </p>
+            </div>
+            <button>Join</button>
+          </div>
         </div>
       </div>
     );
